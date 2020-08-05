4a4bb55d899049f5e1085f5a6a5484fed11779a7
Segeti, Nicolas <nsegeti@gmail.com>
2020-08-05 16:39:53 -0300

    adding Customer methods

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,17 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH = "/api/v1/customer/";
     private String apihost;
 
     private final RestTemplate restTemplate;
 
     public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
     }
 
     public BeerDto getBeerById(UUID uuid){
@@ -38,7 +44,19 @@ public class BreweryClient {
         restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid);
     }
 
-    public void setApihost(String apihost) {
-        this.apihost = apihost;
+    public CustomerDto getCustomerById(UUID uuid){
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH + uuid.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto){
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH, customerDto);
+    }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto){
+        restTemplate.put(apihost + CUSTOMER_PATH + "/" + uuid.toString(), customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid){
+        restTemplate.delete(apihost + CUSTOMER_PATH + "/" + uuid);
     }
 }
